7d99ae5d8af2d0b0a1c5c80b5bd666b3259c8b83
Yang Ke <saint_yk@163.com>
2017-05-22 16:56:03 +0800

    first commit

---
+++ components/AppRoot.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './Header';
+class AppRoot extends React.Component {
+	render() {
+		return <div>
+			<Header />
+         </div>;
+	}
+};
+
+export default AppRoot;
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+// gulpfile.js
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+var less = require('gulp-less');
+var connect = require('connect');
+var serve = require('serve-static');
+var browsersync = require('browser-sync');
+
+gulp.task('build', function() {
+  return browserify({
+      entries: 'app.js',
+      extensions: ['.jsx'],
+      debug: true
+    })
+    .transform(babelify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(browsersync.stream());;
+});
+
+gulp.task('less', function() {
+  return gulp.src('less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./dist/style'))
+    .pipe(browsersync.stream());
+});
+
+gulp.task('compress', function() {
+  return gulp.src('./dist/bundle.js')
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+
+
+gulp.task('browsersync', function() {
+  browsersync({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch("./*.js", ['build']);
+  gulp.watch("./components/*.jsx", ['build']);
+  gulp.watch("./dispatcher/*.js", ['build']);
+  gulp.watch("./stores/*.js", ['build']);
+  gulp.watch("./less/*.less", ['less']);
+  gulp.watch("*.html").on('change', browsersync.reload);
+});
+
+gulp.task('default', function(cb) {
+  runSequence('build', 'less', 'browsersync');
+});
