9ac89d48e55a7f2e314fbf4d0881a60f467572a6
Yang Ke <saint_yk@163.com>
2017-05-23 09:34:20 +0800

    add header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 class Header extends React.Component{
 	render(){
 		return (<div className="header">
-			<h1></h1>
+			<a id="header__icon"></a>
+			<a id="header__logo">
+				<img id="logo" src="/dist/images/pickone_logo.png" />
+			</a>
 		</div>);
 	}
 }
