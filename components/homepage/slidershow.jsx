d10c4dfc3defea46484a1cea5bc10d7db5677079
Yang Ke <saint_yk@163.com>
2017-05-23 16:07:08 +0800

    homepage import react-slick slider

--- components/homepage/slidershow.jsx
+++ components/homepage/slidershow.jsx
@@ -1,30 +1,49 @@
 import React from 'react';
+import Slider from 'react-slick';
 
 class sliderShow extends React.Component{
 	render(){
+		var settings = {
+	      dots: true,
+	      infinite: true,
+	      speed: 2000,
+	      autoplaySpeed: 7000,
+	      slidesToShow: 1,
+	      slidesToScroll: 1,
+	      arrows: false,
+	      dots: false,
+	      autoplay: true,
+	      fade: true,
+	      touchMove: false,
+	      swipe: false,
+	    };
 		return (
-			<div id="slidesshow">
-				<div className="slide">
-					<section className="slideImg"></section>
-					<p className="slideTitle">
-						<span>Pickone 挑场地</span>
-						<br />
-						<span>帮您找到完美活动场地</span>
-						<br />
-						<span className="slideTagLine">希望美好的活动在各处发生，闲置空间都能被看见</span>
-					</p>
+			<Slider classNmae="slidesshow" {...settings}>
+				<div>
+					<div className="slide">
+						<section className="slideImg second"></section>
+						<p className="slideTitle">
+							<span>Pickone 挑场地</span>
+							<br />
+							<span>帮您找到完美活动场地</span>
+							<br />
+							<span className="slideTagLine">希望美好的活动在各处发生，闲置空间都能被看见</span>
+						</p>
+					</div>
 				</div>
-				<div className="slide" style={{"display": "none"}}>
-					<section className="slideImg"></section>
-					<p className="slideTitle">
-						<span>Pickone 挑场地</span>
-						<br />
-						<span>1000间场地 永远免费简单预约</span>
-						<br />
-						<span className="slideTagTitle">希望美好的活动在各处发生，闲置空间都能被看见</span>
-					</p>
+				<div>
+					<div className="slide">
+						<section className="slideImg first"></section>
+						<p className="slideTitle">
+							<span>Pickone 挑场地</span>
+							<br />
+							<span>1000间场地 永远免费简单预约</span>
+							<br />
+							<span className="slideTagLine">希望美好的活动在各处发生，闲置空间都能被看见</span>
+						</p>
+					</div>
 				</div>
-			</div>
+			</Slider>
 		);
 	}
 };
