e1056257ad7cdeedb78205e0b8f37914776f8d48
Yang Ke <saint_yk@163.com>
2017-05-24 17:03:07 +0800

    custom homepage galleryThumb dom structure

--- components/homepage/gallery.jsx
+++ components/homepage/gallery.jsx
@@ -5,6 +5,7 @@ class Gallery extends React.Component{
 	render(){
 		let settings = {
 			dots: false,
+			infinite: false,
 			centerMode: true,
 			touchMove: false,
 	      	swipe: false,
@@ -14,22 +15,67 @@ class Gallery extends React.Component{
 			<section className="gallerywrapper">
 				<Slider  {...settings}>
 					<div>
-						<div className="galleryThumb"></div>
+						<div className="galleryThumb">
+							<h3>课程讲座</h3>
+							<img src="/dist/images/event_icons-01.png" alt="" />
+							<a href="#">查看方案</a>
+						</div>
 					</div>
 			        <div>
-			        	<div className="galleryThumb"></div>
+			        	<div className="galleryThumb">
+			        		<h3>派对</h3>
+			        		<img src="/dist/images/event_icons-03.png" alt="" />
+							<a href="#">查看方案</a>
+			        	</div>
 			        </div>
 			        <div>
-			        	<div className="galleryThumb"></div>
+			        	<div className="galleryThumb">
+			        		<h3>会议</h3>
+			        		<img src="/dist/images/event_icons-05.png" alt="" />
+							<a href="#">查看方案</a>
+			        	</div>
 			        </div>
 			        <div>
-			        	<div className="galleryThumb"></div>
+			        	<div className="galleryThumb">
+			        		<h3>聚会餐饮</h3>
+			        		<img src="/dist/images/event_icons-06.png" alt="" />
+							<a href="#">查看方案</a>
+			        	</div>
 			        </div>
 			        <div>
-			        	<div className="galleryThumb"></div>
+			        	<div className="galleryThumb">
+			        		<h3>拍摄摄影</h3>
+			        		<img src="/dist/images/event_icons-07.png" alt="" />
+							<a href="#">查看方案</a>
+			        	</div>
 			        </div>
 			        <div>
-			        	<div className="galleryThumb"></div>
+			        	<div className="galleryThumb">
+			        		<h3>发表会</h3>
+			        		<img src="/dist/images/event_icons-04.png" alt="" />
+							<a href="#">查看方案</a>
+			        	</div>
+			        </div>
+			        <div>
+			        	<div className="galleryThumb">
+			        		<h3>音乐表演</h3>
+			        		<img src="/dist/images/event_icons-08.png" alt="" />
+							<a href="#">查看方案</a>
+			        	</div>
+			        </div>
+			        <div>
+			        	<div className="galleryThumb">
+			        		<h3>静态展览</h3>
+			        		<img src="/dist/images/event_icons-09.png" alt="" />
+							<a href="#">查看方案</a>
+			        	</div>
+			        </div>
+			        <div>
+			        	<div className="galleryThumb">
+			        		<h3>教育训练</h3>
+			        		<img src="/dist/images/event_icons-02.png" alt="" />
+							<a href="#">查看方案</a>
+			        	</div>
 			        </div>
 				</Slider>
 			</section>
