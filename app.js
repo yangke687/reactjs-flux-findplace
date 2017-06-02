7097165a652a058c985774fec6317a00e2a3f739
Yang Ke <saint_yk@163.com>
2017-06-02 09:58:22 +0800

    import react-router module

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+	Router,
+	Route,
+	hashHistory
+} from 'react-router';
 import AppRoot from './components/AppRoot';
+import AppSearch from './components/AppSearch';
 
 ReactDOM.render(
-	<AppRoot />,
+	(<Router history={hashHistory}>
+		<Route path="/" component={AppRoot} />
+		<Route path="/search" component={AppSearch} />
+	</Router>),
 	document.getElementById('app-root')
 );
