c03d7ea26db9fe7bd65ae721e874234183775bc6
YangKe <saint_yk@163.com>
2017-06-07 13:52:35 +0800

    extract navbar style

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ import {
 } from 'react-router';
 import AppRoot from './components/AppRoot';
 import AppSearch from './components/AppSearch';
+import AppPlace from './components/AppPlace';
 
 ReactDOM.render(
 	(<Router history={hashHistory}>
 		<Route path="/" component={AppRoot} />
+		<Route path="/place/:id" component={AppPlace} />
 		<Route path="/search(/:uses)(/:county)(/:max_people)(/:keywords)" component={AppSearch} />
 	</Router>),
 	document.getElementById('app-root')
---
+++ components/AppPlace.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import NavBar from './search/navbar';
+
+class AppPlace extends React.Component{
+	render(){
+		return (
+			<div className="single-place">
+				<NavBar />
+				<div className="container">
+					<div className="row">
+						
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default AppPlace;
