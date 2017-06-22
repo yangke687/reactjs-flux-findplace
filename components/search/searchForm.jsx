096df362f522a87c564d339fb1632c9120685798
Ke Yang <yangke687@gmail.com>
2017-06-22 13:08:48 +0800

    add select options

--- components/search/searchForm.jsx
+++ components/search/searchForm.jsx
@@ -13,43 +13,55 @@ class SearchForm extends React.Component{
 						<form id="searchForm" onSubmit={this.handleSubmit.bind(this)}>
 							<ul className="nav">
 								<li>
-									<span>目的</span>
+									<span>活动性质</span>
 									<div className="searchSelect">
 										<select className="changeFilter" name="uses">
-               								<option value="">请选择</option>
-                                    	</select>
+               					<option value="">请选择</option>
+												<option value="">体育活动</option>
+												<option value="">派对</option>
+												<option value="">烹饪</option>
+												<option value="">拍摄摄影</option>
+												<option value="">聚会餐饮</option>
+												<option value="">教育训练</option>
+                    </select>
 									</div>
 								</li>
 								<li>
 									<span>活动人数</span>
 									<div className="searchSelect">
 										<select className="changeFilter" name="max_people">
-               								<option value="">请选择</option>
-                                    	</select>
+											<option value="">请选择</option>
+											<option value="1.20">1 - 10</option>	
+											<option value="11.30">11 - 20</option>	
+											<option value="21.60">21 - 40</option>	
+											<option value="41.100">41 - 60</option>	
+											<option value="61.150">61 - 80</option>	
+											<option value="81.200">81 - 100</option>	
+											<option value="101.300">101 - 200</option>	
+											<option value="201.450">201 - 300</option>	
+											<option value="301.500">301 - 400</option>	
+											<option value="401.700">401 - 500</option>	
+											<option value="501">500+</option>	
+                    </select>
 									</div>
 								</li>
 								<li>
 									<span>地点</span>
 									<div className="searchSelect">
 										<select className="changeFilter" name="county">
-               								<option value="">请选择</option>
-                                    	</select>
+											<option value="">请选择</option>
+               				<option value="">昆明市</option>
+                    </select>
 									</div>
 								</li>
 								<li>
-									<span>每小时预算上限</span>
+									<span>租用类别</span>
 									<div className="searchSelect">
 										<select className="changeFilter" name="maxHrPrice">
-               								<option value="">请选择</option>
-                                    	</select>
-									</div>
-								</li>
-								<li>
-									<span>每小时最低预算</span>
-									<div className="searchSelect">
-										<select className="changeFilter" name="minHrPrice">
-               								<option value="">请选择</option>
-                                    	</select>
+               				<option value="">请选择</option>
+											<option value="">包场</option>
+											<option value="">散场</option>
+                    </select>
 									</div>
 								</li>
 								<li>
