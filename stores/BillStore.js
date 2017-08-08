36b075b4b86aab80652bbadb0174806ab4367cde
YangKe <saint_yk@163.com>
2017-08-08 10:21:41 +0800

    Fetch place info in 'AppBill' component

--- stores/BillStore.js
+++ stores/BillStore.js
@@ -1,9 +1,11 @@
 var alt = require('../alt');
 var BillActions = require('../actions/BillActions');
+var PlaceActions = require('../actions/PlaceActions');
 var BillSource = require('../sources/BillSource');
 
 class BillStore {
 	constructor() {
+    this.place = {};
 		this.monthDays = {};
 		this.errorMessage = null;
 
@@ -11,6 +13,9 @@ class BillStore {
 			handleUpdateMonthDays: BillActions.UPDATE_MONTH_DAYS,
 			handledMonthDaysFailed: BillActions.MONTH_DAYS_FAILED,
 			handleSelectMonth: BillActions.SELECT_MONTH,
+      handleFetchPlace: PlaceActions.FETCH_PLACE,
+      handleUpdatePlace: PlaceActions.UPDATE_PLACE,
+      handlePlaceFailed: PlaceActions.PLACE_FAILED,
 		});
 
 		this.exportAsync(BillSource);
@@ -27,6 +32,19 @@ class BillStore {
 	handledMonthDaysFailed(errorMessage) {
 		this.errorMessage = errorMessage;
 	}
+
+  handleFetchPlace() {
+    this.place = {};
+  }
+
+  handleUpdatePlace(place) {
+    this.place = place;
+    this.errorMessage = null;
+  }
+
+  handlePlaceFailed(errorMessage) {
+    this.errorMessage = errorMessage;
+  }
 }
 
 export default alt.createStore(BillStore, 'BillStore');
