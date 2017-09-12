cd3d0f3754c0be638361e74dc502ced931aea572
Yang Ke <saint_yk@163.com>
2017-09-12 15:03:47 +0800

    Fetch wechat returned 'code'

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-	Router,
-	Route,
-	hashHistory
-} from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
+import URI from 'urijs';
+
+import { APP_ID, APP_SECRET } from './sources/config';
 import AppRoot from './components/AppRoot';
 import AppSearch from './components/AppSearch';
 import AppPlace from './components/AppPlace';
 import AppBill from './components/AppBill';
 import AppPay from './components/AppPay';
 
+const generateGetCodeUrl = function(redirectURL) {
+	return new URI("https://open.weixin.qq.com/connect/oauth2/authorize")
+        .addQuery("appid", APP_ID)
+        .addQuery("redirect_uri", redirectURL)
+        .addQuery("response_type", "code")
+        .addQuery("scope", "snsapi_userinfo")
+        .addQuery("response_type", "code")
+        .hash("wechat_redirect")
+        .toString();
+}
+
+const wechatAuth = function(nextState, replace, next) {
+	const uri = new URI(document.location.href);
+	const query = uri.query(true);
+	const {code} = query;
+	
+	if(code) {
+			//WechatUserStore.fetchUserInfo(code);
+			console.log(code);
+			next();
+	} else {
+			document.location = generateGetCodeUrl(document.location.href);
+	}
+}
+
 ReactDOM.render(
-	(<Router history={hashHistory}>
-		<Route path="/" component={AppSearch} />
+	(<Router history={browserHistory}>
+		<Route path="/" 
+			onEnter={wechatAuth.bind(this)}
+			component={AppSearch} />
 		<Route path="/place/:id" component={AppPlace} />
 		<Route path="/bill/:id" component={AppBill} />
 		<Route path="/search(/:uses)(/:county)(/:max_people)(/:keywords)" component={AppSearch} />
--- sources/config.js
+++ sources/config.js
@@ -2,3 +2,5 @@
 //export const API_URL = 'http://gfwy.wyglpt.com/wechat/placeRent/';
 //export const API_URL = 'http://127.0.0.1/wechat/placeRent/';
 export const API_URL = '/api';
+export const APP_ID = 'wx98e0fd7bbcc922a1';
+export const APP_SECRET = '228ddee97326d7abff0a5a8c130f1756';
