125d4f1eb6c2d23b41b14ca5057e5cba346308fa
Yang Ke <saint_yk@163.com>
2017-09-18 13:25:21 +0800

    Set 'PUBLIC_PATH' setting

--- sources/config.js
+++ sources/config.js
@@ -1,6 +1,14 @@
 //export const API_URL = 'http://192.168.17.159//wechat/placeRent';
-//export const API_URL = 'http://gfwy.wyglpt.com/wechat/placeRent/';
+export const API_URL = 'http://gfwy.wyglpt.com/wechat/placeRent/';
 //export const API_URL = 'http://127.0.0.1/wechat/placeRent/';
-export const API_URL = '/api';
-export const APP_ID = 'wx98e0fd7bbcc922a1';
-export const APP_SECRET = '228ddee97326d7abff0a5a8c130f1756';
+//export const API_URL = '/api';
+
+export const PUBLIC_PATH = '/place-rent/';
+
+// test wechat
+//export const APP_ID = '';
+//export const APP_SECRET = '';
+
+// gfwy wechat
+export const APP_ID = 'wxe46dec908570bdc4';
+export const APP_SECRET = '253f079ac0a39a51160ac83907605210';
